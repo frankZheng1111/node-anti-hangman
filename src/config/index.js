876ac51787e003ae64f96105a46b0b582f3276b2
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-04-20 22:22:40 +0800

    Add some config

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,6 @@
 'use strict'
 const ENV = process.env.NODE_ENV || 'dev'
 
-export const logger = require('../../config/logger.json')
+export const logger = require('../../config/logger.json');
+export const user = require('../../config/user.json')[ENV];
+export const hangman = require('../../config/hangmanServer.json')[ENV];
