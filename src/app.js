3a4eb7fd4232465593fb7c276c5c1bfb61fbe570
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-04-22 12:11:13 +0800

    Complete a simple base flow

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 'use strict'
+import logger from './libs/logger';
 import User from './models/user';
 import Hangman from './models/hangman';
 import { user as userConfig } from './config';
 
-let user = new User(userConfig.name, userConfig.password).signin()
-  .then((data) => {
-    new Hangman(data.cookie).newGame()
-    .then((data) => {
-      console.log(data);
+new User(userConfig.name, userConfig.password).signin()
+  .then((user) => {
+    new Hangman(user.cookie).newGame()
+    .then((hangman) => {
+      return hangman.guessingWord();
+    })
+    .then((hangman) => {
+      logger.info(`STATE: ${hangman.state}, HP: ${hangman.hp}, CURRENTWORD: ${hangman.currentWordStr}`);
     });
   });
--- src/models/hangman.js
+++ src/models/hangman.js
@@ -3,10 +3,25 @@ import request from 'request-promise';
 import logger from '../libs/logger';
 import { hangmanServer } from '../config';
 
+const HANGMAN_FINSIHED_STATES = [ 'win', 'lose', 'giveup' ];
+let letters = 'abcdefghijklmnopqrstuvwxyz'.split('')
+
 class Hangman {
   constructor(cookie) {
     console.log(cookie)
     this.cookie = cookie;
+    this.id = "";
+    this.hp = -1;
+    this.state = "";
+    this.currentWordStr = "";
+  }
+
+  refreshHangmanByResp(hangman) {
+    this.id = hangman.data.id;
+    this.hp = hangman.data.attributes.hp;
+    this.state = hangman.data.attributes.state;
+    this.currentWordStr = hangman.data.attributes.currentWordStr;
+    return this;
   }
 
   newGame() {
@@ -20,7 +35,43 @@ class Hangman {
       },
       json: true
     }
-    return request(options);
+    return request(options)
+      .then((hangman) => {
+        this.refreshHangmanByResp(hangman);
+        logger.info(`START NEW GAME: ${this.id}`);
+        return Promise.resolve(this);
+      });
+  }
+  
+  guessingWord() {
+    letters.pop();
+    if (HANGMAN_FINSIHED_STATES.includes(this.state)) {
+      return Promise.resolve(this);
+    } else {
+      return this.guess(letters[letters.length - 1])
+        .then(() => {
+          return this.guessingWord();
+        });
+    }
+  }
+
+  guess(letter) {
+    let options = {
+      method: 'patch',
+      uri: `${hangmanServer.url}/api/hangmen/${this.id}`,
+      form: {
+        letter: letter
+      },
+      headers: {
+        Cookie: this.cookie
+      },
+      json: true
+    }
+    return request(options).then((hangman) => {
+      this.refreshHangmanByResp(hangman);
+      logger.info(`ID: ${this.id}, GUESS: ${letter}, HP: ${this.hp}, CURRENTWORD: ${this.currentWordStr}`);
+      return Promise.resolve(this);
+    });
   }
 }
 
