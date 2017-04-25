9a352b9e7fd7c63e25accca7f73dbe9b0e90bb14
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-04-25 22:52:03 +0800

    Add one test

--- test/models/hangmen.js
+++ test/models/hangmen.js
@@ -1,4 +1,17 @@
-'use strict'
+'use strict';
 import { should as _should } from 'chai';
+// import sinon from 'sinon';
+import Hangman from '../../src/models/hangman.js';
+
+// eslint-disable-next-line
 const should = _should();
+
+describe('Test Hangman class', () => {
+  it('should new hangman and constructor should be run', () => {
+    let hangman = new Hangman('cookie', 'k');
+    hangman.cookie.should.equal('cookie');
+    hangman.nextLetter.should.equal('k');
+  });
+});
+
 
